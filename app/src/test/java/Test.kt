9931b4f490f34767738535c99c23366d731a3e16
lyoun3060 <omega3060@naver.com>
2023-05-10 09:32:54 +0900

    23.5/10 학원 1교시

---
+++ app/src/test/java/Test.kt
@@ -0,0 +1,12 @@
+//최상위 영역
+// java에서는 int num = 1;형식으로 썻으나
+// 코틀린에서는 val(or var) 변수명 : 타입 = 값;
+val num : Int = 1; //해당 함수 사용하면 색, 사용안하면 회색
+
+class Test {
+}
+
+fun main(){
+    println("hi android")
+    println("num의 값은 : $num")
+}
