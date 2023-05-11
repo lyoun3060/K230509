1f14d982f5befec5ab2f76e9ab749bb2488230b2
lyoun3060 <omega3060@naver.com>
2023-05-11 12:31:45 +0900

    23.5/11 4교시 중간에 업데이트

--- test6/src/main/java/com/example/test6/MainActivity.kt
+++ test6/src/main/java/com/example/test6/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.test6
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.test6.databinding.ActivityMain2Binding
+import com.example.test6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() { //AppCompatActivity() 부모클래스로 부터 상속을 받음
     //코드 작업 공간임,
@@ -15,5 +21,47 @@ class MainActivity : AppCompatActivity() { //AppCompatActivity() 부모클래스
         //리소스 R.java파일이라는 곳에 상수값으로 저장되는데, 여기서 불러옵니다.
         //그래서, 코드에서 리소스 값을 불러올때 항상 R.layout 등으로 시작을해서 불러옴
         // 따라서 이코드는 화면을 출력하겠다는 코드, 레이아웃(뷰 그룹: 뷰(텍스트, 이미지, 인풋, 체크박스, 라디오..)를 모아주는 역할
-    }
+
+
+        //뷰 바인딩 기술을 통해서, 뷰들을, 특정 바인딩이라는 객체에 모두 모아서 접근을 쉽게 해주는 기술
+        //사용하기 전에 항상, build.gradle 파일에 설정후, sync now를 적용해서 사용해야함
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        //이 문법은 자동으로 생성된 바인딩 파일을 inflate함수와, 인자는:layoutInflater로 고정
+        //setContentView = 화면에 그리기
+        //binding 변수에 모든 뷰가 다 들어가 있음
+        //견론은 여기서 필요한 뷰를 꺼내서 ㅅ용합니다.
+        setContentView(binding.root)
+
+        //button1을 뷰바인딩 기법으로 접근
+        var status = 0
+        binding.btn1.setOnClickListener{
+
+            if(status == 0){
+                binding.img1.visibility = View.GONE
+                status = 1
+            }else {
+                binding.img1.visibility = View.VISIBLE
+                status = 0
+            }
+
+        }
+        }
+
+//        val button1 = findViewById<Button>(R.id.btn1)
+//        val image1 = findViewById<ImageView>(R.id.img1)
+//        var status = 0
+//
+//        //button1 눌러서 -> image1 show/hide
+//        button1.setOnClickListener{
+//            if(status == 0){
+//                image1.visibility = View.GONE
+//                status = 1
+//            }else {
+//                image1.visibility = View.VISIBLE
+//                status = 0
+//            }
+//
+//        }
+//
+//    }
 }
