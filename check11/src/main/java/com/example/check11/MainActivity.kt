eef657e7a4c5df2651f4a916ec23117ed1952903
lyoun3060 <omega3060@naver.com>
2023-05-16 17:00:09 +0900

    5/16 check11 - 11장 실습 중

--- check11/src/main/java/com/example/check11/MainActivity.kt
+++ check11/src/main/java/com/example/check11/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.check11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.check11.databinding.ActivityMainBinding
+import com.example.check11.databinding.ItemRecyclerviewBinding
 
 class MainActivity : AppCompatActivity() {
+    //test11에 MA5.kt참조
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val datas = mutableListOf<String>()
+        for(i in 1..9){
+            datas.add("Item $i")
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(MyDecoration(this))
+
+    }
+
+    class MyViewHolder(val binding: ItemRecyclerviewBinding):RecyclerView.ViewHolder(binding.root)
+
+    class MyAdapter(val datas:MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
     }
 }
